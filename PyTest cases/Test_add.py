0de2f044710a4ea80d20bb88717abad83edf0fcb
Anand Singh <anandrawat819@gmail.com>
2025-07-31 07:53:03 -0700

    for calculator of addtion

--- PyTest cases/Test_add.py
+++ PyTest cases/Test_add.py
@@ -1,6 +1,16 @@
-from add import add,multi
-def test_add():
-    assert add(2, 4) ==6
+# from add import add,multi
+# def test_add():
+#     assert add(2, 4) ==6
+#
+# def test_mul():
+#     assert multi (2,5)==10
 
-def test_mul():
-    assert multi (2,5)==10
+def test_craditcard():
+    message="hello"
+    assert message=="hello"
+
+def test_second():
+    a=10
+    b=20
+
+    assert a+2==12
